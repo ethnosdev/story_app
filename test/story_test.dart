988fd5aa73a87ed98065c0e63037fa68686bb3d1
Macbook pro <macbookpro@192.168.1.10>
2024-08-01 14:36:28 +0000

    Complete two issue

--- test/story_test.dart
+++ test/story_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:story/story.dart';
+import 'package:story/story_app.dart';
 
 void main() {
   test('adds one to input values', () {
