adc4eaaabd823c23853d1c925cf3673dbb342bd4
Junior Djedje <adjedje595@gmail.com>
2024-09-02 16:40:28 +0000

    error corrected in story_list_title

--- lib/src/story_list_tile.dart
+++ lib/src/story_list_tile.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import "../story_app.dart";
 class StoryListTile extends StatelessWidget {
   const StoryListTile({super.key,required this.storyNumber,required this.storyTitle});
   final int storyNumber;
   final String storyTitle;
   @override
   Widget build(BuildContext context) {
-    return const StoryApp();
+    return ListTile(
+          leading: Text("$storyNumber"),
+          title: Text("Story $storyTitle"),
+        );
   }
 }
--- lib/story_app.dart
+++ lib/story_app.dart
@@ -1,7 +1,13 @@
-library story;
+import 'package:flutter/material.dart';
+import "./src/story_list_tile.dart";
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class StoryApp extends StatelessWidget {
+  const StoryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        itemBuilder: (context,idx)=> StoryListTile(storyNumber: idx+1, storyTitle: "Story ${idx+1}"),
+      separatorBuilder: (context,idx)=>const SizedBox(height: 5,), itemCount: 10);
+  }
 }
