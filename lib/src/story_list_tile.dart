013824ba1b808116f671068211901180d0f0f60c
Junior Djedje <adjedje595@gmail.com>
2024-09-11 09:48:45 +0000

    Improve StoryListTile

--- lib/src/story_list_tile.dart
+++ lib/src/story_list_tile.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 class StoryListTile extends StatelessWidget {
-  const StoryListTile({super.key,required this.storyNumber,required this.storyTitle});
-  final int storyNumber;
-  final String storyTitle;
+  const StoryListTile({super.key,this.number,required this.title});
+  final int? number;
+  final String title;
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-          leading: Text("$storyNumber"),
-          title: Text("Story $storyTitle"),
-        );
+    return InkWell(
+      onTap: (){print("Click me");},
+      child: ListTile(
+            leading: number != null ? Text("$number",style: TextStyle(fontSize: 16)) : null,
+            title: Text("Story $title",style: TextStyle(fontSize: 16)),
+          ),
+    );
   }
 }
